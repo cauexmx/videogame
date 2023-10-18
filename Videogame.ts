59966357ab75bd4bdddc0fe9f3243baac37acb8c
cauexmx <127966350+cauexmx@users.noreply.github.com>
2023-10-18 14:28:01 -0300

    Add files via upload

---
+++ Videogame.ts
@@ -0,0 +1,53 @@
+class Videogame {
+    name:string;
+    gender:string;
+    numPlayers:number;
+    plataform:[string];
+    preco:number;
+
+    constructor(name:string, gender:string, numPlayers:number, plataform:[string], preco:number){
+        this.name = name;
+        this.gender = gender;
+        this.numPlayers = numPlayers;
+        this.plataform = plataform;
+        this.preco = preco;
+    }
+
+    showDetails():void{
+        console.log(`O jogo escolhido foi ${this.name}`)
+    }
+
+    Apply():void{
+        console.log(`Seu desconto é de 10%, portanto o novo preço será ${this.preco*0.9}.`)
+    }
+    
+    novaPlataforma(novaPlataforma : string): [string] {
+        this.plataform.push(novaPlataforma)
+        return this.plataform
+
+    }
+
+    estimarTempoJogo(): string {
+        switch (this.gender) {
+          case 'Ação':
+            return 'Cerca de 12 horas';
+          case 'Aventura':
+            return 'Cerca de 20 horas';
+          case 'Esportes':
+            return 'Indeterminado';
+          case 'Estratégia':
+            return 'Cerca de 40 horas';
+          default:
+            return 'Tempo estimado desconhecido';
+        }
+    }
+}
+
+const sonic = new Videogame('Sonic', 'Aventura', 2, ['Nintendo'] , 50);
+sonic.showDetails();
+sonic.Apply();
+
+sonic.novaPlataforma('PS4')
+console.log(sonic.plataform)
+console.log(`Tempo estimado para terminar o jogo: ${sonic.estimarTempoJogo()}`);
+
